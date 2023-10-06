7c88440e105c540f82be1404e149f0a9110f8e11
Karsten Witt <karsten.witt@decisions.com>
2023-10-06 15:53:36 -0400

    [int] to version 8.16.0 min and another guess path for BuildModule.ps1

--- BuildModule.ps1
+++ BuildModule.ps1
@@ -6,6 +6,11 @@ param (
 )
 
 function FindMSBuild {
+    $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
+    if (Test-Path -PathType leaf -LiteralPath $guess ) {
+        return $guess
+    }
+
     $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe"
     if (Test-Path -PathType leaf -LiteralPath $guess ) {
         return $guess
